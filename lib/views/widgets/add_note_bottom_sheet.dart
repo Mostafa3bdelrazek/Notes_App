1e4fa4c141e9710678523ac818c134d45776aedf
mostafa abdelrazek <mostafaabdelrazrk.com>
2023-09-19 02:11:51 +0300

    create custom text form field

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/widgets/custom_text_form_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          const SizedBox(height: 32),
+          CustomTextField(
+            labelText: 'title',
+          ),
+          const SizedBox(height: 16),
+          CustomTextField(
+            labelText: 'content',
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/custom_text_form_field.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+// ignore: must_be_immutable
+class CustomTextField extends StatelessWidget {
+  String? labelText;
+  Function(String)? onChanged;
+  bool obscureText;
+  CustomTextField({
+    super.key,
+    this.onChanged,
+    this.labelText,
+    this.obscureText = false,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: (data) {
+        if (data!.isEmpty) {
+          return 'field is required';
+        }
+        return null;
+      },
+      obscureText: obscureText,
+      onChanged: onChanged,
+      cursorColor: primaryColor,
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: const TextStyle(color: primaryColor),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: const BorderSide(
+            color: primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+//=================================
+// TextFormField(
+//                   validator: (value) {
+//                     if (value!.isEmpty) {
+//                       return 'Password must be not empty';
+//                     }
+//                     return null;
+//                   },
+//                   controller: passwordController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Password',
+//                     prefixIcon: const Icon(
+//                       Icons.lock,
+//                     ),
+//                     suffixIcon: IconButton(
+//                       onPressed: () {
+//                         setState(() {
+//                           view = !view;
+//                           if (view == true) {
+//                             icon = Icons.remove_red_eye;
+//                           } else {
+//                             icon = Icons.visibility_off;
+//                           }
+//                         });
+//                       },
+//                       icon: Icon(
+//                         icon,
+//                       ),
+//                     ),
+//                     enabledBorder: const OutlineInputBorder(
+//                       borderSide: BorderSide(
+//                         color: Colors.white,
+//                       ),
+//                     ),
+//                   ),
+//                   keyboardType: TextInputType.visiblePassword,
+//                   obscureText: view,
+//                 ),
